1133f6f2edba99554b983292115439d74641611e
James Wilcock <jameswilcock8@gmail.com>
2023-12-31 18:27:24 -0800

    got instancing working with only drawing unique colors

--- main.c
+++ main.c
@@ -2,8 +2,31 @@
 #include <raymath.h>
 #define RLIGHTS_IMPLEMENTATION
 #include "rlights.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define MAX_SAMPLES 1000000
+#define MAX_COLORS 50000
+#define CUBE_SIDE_LEN 0.05f
+
+uint8_t *drawn_pixel_map;
+Color *color_list;
+Vector3 *cubePosition;
+
+bool color_in_list(Color cur_color) {
+  // use cur color to see what to check
+  unsigned int index = (cur_color.r / 255.0f) + (cur_color.g / 255.0f) * (256) +
+                       (cur_color.b / 255.0f) * (256 * 256);
+  unsigned int byte_index = index / (8);
+  unsigned int cur_bitfield = drawn_pixel_map[byte_index];
+  bool present = (cur_bitfield >> (index - (byte_index * 8))) & 0x1;
+  if (!present) {
+    // need to set in pixel map
+    drawn_pixel_map[byte_index] |= 1 << (index - (byte_index * 8));
+  }
+  return present;
+}
 
 int main(int argc, char *argv[]) {
   // goal is to load image
@@ -13,6 +36,12 @@ int main(int argc, char *argv[]) {
   const int scr_width = 800;
   const int scr_height = 600;
   const float circle_radius = 0.1;
+  const float graph_limit = 5;
+  Image target_image = LoadImage(filename);
+
+  drawn_pixel_map = malloc((256 * 256 * 256) / (8 * sizeof(uint8_t)));
+  color_list = malloc(target_image.height * target_image.width * sizeof(Color));
+
   InitWindow(scr_width, scr_height,
              "raylib [models] example - model animation");
   Camera camera = {0};
@@ -25,7 +54,6 @@ int main(int argc, char *argv[]) {
 
   Vector3 center = {0, 0, 0};
 
-  Image target_image = LoadImage(filename);
   Color *target_colors = LoadImageColors(target_image);
   // maybe instead need to find the unique pixels?
   // to reduce number needed
@@ -34,7 +62,7 @@ int main(int argc, char *argv[]) {
 
   Mesh my_pyr = GenMeshPoly(4, 1);
   Mesh my_cube = GenMeshCube(1.0f, 1.0f, 1.0f);
-  Mesh my_small_cube = GenMeshCube(0.1f, 0.1f, 0.1f);
+  Mesh my_small_cube = GenMeshCube(CUBE_SIDE_LEN, CUBE_SIDE_LEN, CUBE_SIDE_LEN);
   Material matIntances = LoadMaterialDefault();
   Matrix *transforms = (Matrix *)RL_CALLOC(num_pixels, sizeof(Matrix));
   Material matDefault = LoadMaterialDefault();
@@ -61,14 +89,28 @@ int main(int argc, char *argv[]) {
   Material matInstances = LoadMaterialDefault();
   matInstances.shader = shader;
   matInstances.maps[MATERIAL_MAP_DIFFUSE].color = RED;
+  printf("making color list\n");
 
-  for (int i = 0; i < num_pixels; i++) {
+  int color_cnt = 0;
+  printf("randomly populating color list\n");
+  for (int i = 0; i < MAX_SAMPLES; i++) {
+    if (color_cnt > MAX_COLORS) {
+      break;
+    }
+    Vector2 coord = {rand() % target_image.width, rand() % target_image.height};
+    Color color = GetImageColor(target_image, coord.x, coord.y);
+    if (!color_in_list(color)) {
+      color_list[color_cnt++] = color;
+    }
+  }
+  printf("found %d unique colors\n", color_cnt);
+
+  for (int i = 0; i < color_cnt; i++) {
     Color cur_color = target_colors[i];
     Vector3 pos;
     pos.x = (double)cur_color.r / 255 * 5;
     pos.y = (double)cur_color.g / 255 * 5;
     pos.z = (double)cur_color.b / 255 * 5;
-    //    printf("%f,%f,%f\n", pos.x, pos.y, pos.z);
     Matrix translation = MatrixTranslate(pos.x, pos.y, pos.z);
     Vector3 axis = Vector3Normalize((Vector3){0, 0, 0});
     Matrix rotation = MatrixRotate(axis, 0);
@@ -102,7 +144,7 @@ int main(int argc, char *argv[]) {
     DrawCylinderEx((Vector3){0, 0, 0}, (Vector3){5, 0, 0}, .1, .1, 12, RED);
     DrawCylinderEx((Vector3){0, 0, 0}, (Vector3){0, 5, 0}, .1, .1, 12, GREEN);
     DrawCylinderEx((Vector3){0, 0, 0}, (Vector3){0, 0, 5}, .1, .1, 12, BLUE);
-    DrawMeshInstanced(my_small_cube, matInstances, transforms, num_pixels);
+    DrawMeshInstanced(my_small_cube, matInstances, transforms, color_cnt);
 
     EndMode3D();
 
