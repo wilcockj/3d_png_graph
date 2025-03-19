a07e5cf9b47063202c6582379e3110c1085f5208
James Wilcock <jameswilcock8@gmail.com>
2025-03-18 21:59:50 -0700

    added header files oops

---
+++ colors.h
@@ -0,0 +1,4 @@
+#pragma once
+
+const char *find_closest_color(unsigned char r, unsigned char g,
+                               unsigned char b);
---
+++ colorutil.h
@@ -0,0 +1,27 @@
+#include <raylib.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+struct image_info {
+  size_t color_cnt;
+  size_t num_pixels;
+  Image *target_image;
+  Texture2D *target_texture;
+  Matrix **transform_list;
+  Color *color_list;
+  uint8_t *drawn_pixel_map;
+  Color *palette;
+  size_t palette_len;
+};
+
+void Draw_Image_In_Region(Texture2D tex, Rectangle region);
+
+bool color_in_list(Color cur_color, uint8_t *drawn_pixel_map);
+
+int populate_color_list(Image target_image, Color *color_list,
+                        uint8_t *drawn_pixel_map);
+
+void load_transforms_from_color_list(Matrix *transform_list, Color *color_list,
+                                     int color_cnt, unsigned int quadrant);
+
+void process_image(struct image_info *info, Image target_image);
